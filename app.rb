c4bd451e44242ec1d5c4ed67a7fe554669e55541
Matthew Solarski <mxtthxw@gmail.com>
2016-04-28 12:14:22 -0400

    Updated Gemfile, fixed profile section, created Heroku site joketown

--- app.rb
+++ app.rb
@@ -6,11 +6,19 @@ require 'rack-flash'
 require 'paperclip'
 require './models'
 
-set :database, "sqlite3:joketown.sqlite3"
-set :sessions, true
-
 enable :sessions
 use Rack::Flash, :sweep => true
+
+configure(:development){set :database, "sqlite3:joketown.sqlite3"}
+# configure(:production){set :database, ""}
+set :sessions, true
+
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
+end
 
 get '/' do
 	# current_user
@@ -19,20 +27,37 @@ get '/' do
 end
 
 get '/welcome' do
+	current_user
 	erb :welcome
 end
 
 get '/landing' do
+	current_user
 	erb :landing
 end
 
 get '/profile' do
+	current_user
+	puts params.inspect
+	puts @current_user
 	erb :profile
 end
 
 post '/sign_up' do
-	user = User.create(params[:signup])
+	@user = User.create(params[:signup])
 	redirect "/welcome"
+end
+
+post '/newprofile' do
+	# puts params.inspect
+	@user = User.find(session[:user_id])
+	@user.firstname = params[:profile][:firstname]
+	@user.lastname = params[:profile][:lastname]
+	@user.age = params[:profile][:age]
+	@user.birthday = params[:profile][:birthday]
+	@user.username = params[:profile][:username]
+	# conditional to check if username is unique
+	redirect '/profile'
 end
 
 post '/sign-in' do
